01e93d2ec2b7d64a3f9176b427e49118c833ab63
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-08 00:36:36 +0100

    encode the password before hashing and added is valid function to check validity

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,4 +14,17 @@ def hash_password(password: str) -> bytes:
     Returns:
         str: the byte encrypted data
     """
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    check if a password is the same with the hashed pass.
+    Args:
+        hashed_password (bytes): password in bytes
+        password (str): in string password
+
+    Returns:
+        bool: True or False
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
