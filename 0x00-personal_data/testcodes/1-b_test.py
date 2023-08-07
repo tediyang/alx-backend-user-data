bfe2185773d3621c219124dd5de1089c2b00535e
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-08 00:36:57 +0100

    testcodes

---
+++ 0x00-personal_data/testcodes/1-b_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+test is valid function
+"""
+
+hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
+
+password = "MyAmazingPassw0rd"
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
