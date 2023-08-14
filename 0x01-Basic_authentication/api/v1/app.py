74cfd4232ca46ef195bd1c696f0116e05124d147
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-14 18:12:28 +0100

    added basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
-if getenv("AUTH_TYPE"):
+if getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
