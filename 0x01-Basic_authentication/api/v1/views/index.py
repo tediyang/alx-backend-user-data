dde1c5777d823401fb86a92cc7dfc44eab8f7369
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-14 12:04:17 +0100

    added endpoint for unauthorized access

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    abort with 401 when accessed
+    """
+    abort(401)
