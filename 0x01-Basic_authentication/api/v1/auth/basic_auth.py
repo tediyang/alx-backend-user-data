aa47fb39e63b6001f48befdf07ff699920916c0f
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-14 18:27:26 +0100

    added new header extrach base64 ....

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ File that create a Basic Authentication Model
 """
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
@@ -12,3 +13,20 @@ class BasicAuth(Auth):
         Auth (TypeVar('Auth')): the authentication model
     """
     pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        base64 authorization header
+
+        Args:
+            authorization_header (str): the string that start with basic.
+
+        Returns:
+            str: the base64 string
+        """
+        if not authorization_header or type(authorization_header) != str:
+            return None
+
+        fetched = re.search(r'Basic\s+(.*)', authorization_header)
+        if fetched:
+            return fetched.group(1)
+        return None
