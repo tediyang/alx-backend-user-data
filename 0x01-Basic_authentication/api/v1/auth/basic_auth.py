46a0cf23c79be6665f7b1698e44d98e0309e1e74
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-14 18:13:22 +0100

    created basic_auth model

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" File that create a Basic Authentication Model
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Implementation of Basic Authentication Model
+
+    Args:
+        Auth (TypeVar('Auth')): the authentication model
+    """
+    pass
