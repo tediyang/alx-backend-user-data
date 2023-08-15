aab645d265eed73fb41408b7ab2fc889dfec4973
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-15 16:28:23 +0100

    added session_auth to flask

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,6 +18,9 @@ auth = None
 if getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
