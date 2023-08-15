b5ace9103ee25685a0ccf1faededf489205c39b2
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-15 16:29:17 +0100

    added SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" File that create a Session Authentication Model
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Implementation of Session Authentication Model
+
+    Args:
+        Auth (TypeVar('Auth')): the authentication model
+    """
+    pass
