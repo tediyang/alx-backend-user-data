106cf41163d50c62414435be43bd348548f536cd
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-15 16:45:30 +0100

    added method to fetch user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,11 +24,25 @@ class SessionAuth(Auth):
         Returns:
             str: returns the session id
         """
-        if not user_id:
-            return None
-        if not isinstance(user_id, str):
+        if not user_id or not isinstance(user_id, str):
             return None
 
         sess_id = uuid4()
         self.user_id_by_session_id[sess_id] = user_id
         return sess_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+         returns a user id based on a session id
+
+        Args:
+            session_id (str, optional): the session id of the user.
+            Defaults to None.
+
+        Returns:
+            str: the user id linked to the provided session id
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
