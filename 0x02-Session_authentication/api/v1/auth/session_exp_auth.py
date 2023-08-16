3352f213fdd0d4770bd9c07ff6766529af59e4b3
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-16 03:07:34 +0100

    create a session expiration model

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+""" create a session expiration
+"""
+from os import getenv
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """
+    This model create an expiration time for the session
+    """
+    def __init__(self):
+        """ initialze the variable
+        """
+        self.session_duration = int(getenv('SESSION_DURATION', 0))
+
+    def create_session(self, user_id=None) -> str:
+        """
+        create a session by calling create session from super and returning
+        the session id.
+
+        Args:
+            user_id (_type_, optional): the user id. Defaults to None.
+
+        Returns:
+            str: the session id
+        """
+        sess_id = super().create_session(user_id)
+        if not sess_id:
+            return None
+
+        self.user_id_by_session_id[sess_id] = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+
+        return sess_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        find and return the user_id from the session dictionary.
+
+        Args:
+            session_id (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            returns user_id
+        """
+        if not session_id:
+            return None
+
+        sess_dict = self.user_id_by_session_id.get(session_id)
+        if not sess_dict:
+            return None
+
+        if self.session_duration <= 0:
+            return sess_dict.get('user_id')
+
+        created_time = sess_dict.get('create_at')
+        if not created_time:
+            return None
+
+        session_elapsed = timedelta(seconds=self.session_duration)
+
+        if created_time + session_elapsed < datetime.now():
+            return None
+
+        return sess_dict.get('user_id')
