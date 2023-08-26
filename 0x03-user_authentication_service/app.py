a723c7ff1815a08a36f00aaa31a7f753ec51dd2a
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-27 00:33:53 +0100

    implemented login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
     A basic flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -32,6 +32,23 @@ def users():
                             "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    log into an existing account
+    """
+    email: str = request.form.get("email")
+    password: str = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        sess_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", sess_id)
+        return response
+
+    abort(401)
 
 
 if __name__ == "__main__":
