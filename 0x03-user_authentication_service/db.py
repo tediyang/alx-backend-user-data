20653860be546a7148b177c2e299edef734d565b
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-26 19:05:10 +0100

    setup database

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        create user and add to the database
+
+        Args:
+            email (str): new user email
+            hashed_password (str): new user password
+        """
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        self._session.add(new_user)
+        self._session.commit()
+
+        return new_user
