a03cf3122d03ee7656230e515c4db65c1051916e
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-27 01:26:28 +0100

    get reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,21 @@ def profile():
         return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', strict_slashes=False)
+def get_reset_password_token():
+    """
+    Fetch reset token for the current user
+    """
+    user_email = request.form.get('email')
+    is_registered = AUTH.create_session(user_email)
+
+    if not is_registered:
+        abort(403)
+
+    token = AUTH.get_reset_password_token(user_email)
+    message = {"email": user_email, "reset_token": token}
+    return jsonify(message)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
